7cfb7ed9a382c1b18deb81b8d4d5770d29a52fb5
Vincent Ambo <tazjin@gmail.com>
2016-09-17 23:20:13 +0200

    feat Route53: Implement adding of Route53 records
    This implements the Route53Responder correctly.
    
    It will first attempt to find the correct hosted zone to use for the
    challenge record and then add the record to that zone.
    
    If no zone is found it will fail. A specific zone may be configurable
    in the future.

--- src/main/java/in/tazj/k8s/letsencrypt/acme/Route53Responder.java
+++ src/main/java/in/tazj/k8s/letsencrypt/acme/Route53Responder.java
@@ -1,9 +1,23 @@
 package in.tazj.k8s.letsencrypt.acme;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.route53.AmazonRoute53;
 import com.amazonaws.services.route53.AmazonRoute53Client;
+import com.amazonaws.services.route53.model.Change;
+import com.amazonaws.services.route53.model.ChangeAction;
+import com.amazonaws.services.route53.model.ChangeBatch;
+import com.amazonaws.services.route53.model.ChangeResourceRecordSetsRequest;
+import com.amazonaws.services.route53.model.HostedZone;
+import com.amazonaws.services.route53.model.RRType;
+import com.amazonaws.services.route53.model.ResourceRecord;
+import com.amazonaws.services.route53.model.ResourceRecordSet;
 
+import java.util.Optional;
+
+import in.tazj.k8s.letsencrypt.util.LetsencryptException;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -12,14 +26,54 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Route53Responder implements DnsResponder {
   final AmazonRoute53 route53 = new AmazonRoute53Client(new DefaultAWSCredentialsProviderChain());
-  final String
-  public void addRecord(String domain, String challenge) {
-
-  }
 
   @Override
   public void addChallengeRecord(String recordName, String challengeText) {
-    // final String challengeDomain = "_acme-challenge." + domain;
-    log.info("Updated challenge for {}", recordName);
+    final Optional<HostedZone> hostedZone = findHostedZone(recordName);
+
+    if (hostedZone.isPresent()) {
+      log.info("Creating record {} in zone {}", recordName, hostedZone.get().getName());
+      final String zoneId = hostedZone.get().getId();
+      final ResourceRecordSet recordSet = new ResourceRecordSet(recordName, RRType.TXT)
+          .withTTL(300L)
+          .withResourceRecords(new ResourceRecord(challengeText));
+      final ChangeBatch changeBatch = new ChangeBatch(ImmutableList.of(
+          new Change(ChangeAction.UPSERT, recordSet)));
+
+      route53.changeResourceRecordSets(new ChangeResourceRecordSetsRequest(zoneId, changeBatch));
+    } else {
+      log.error("No hosted zone found for record: {}!", recordName);
+      throw new LetsencryptException("No hosted zone found");
+    }
+  }
+
+  /** If no hosted zone is supplied explicitly this function will attempt to find a managed zone in
+   * AWS that matches the supplied record.
+   * If multiple matching zones are found the most specific one is chosen. */
+  @VisibleForTesting
+  public Optional<HostedZone> findHostedZone(String record) {
+    // Determine whether the requested domain has a suffixed full stop, and otherwise add it as the
+    // hosted zones returned by Route53 will always contain it.
+    final String fqdnRecord;
+    if (record.endsWith(".")) {
+      fqdnRecord = record;
+    } else {
+      fqdnRecord = record + ".";
+    }
+
+    // Attempt to find the correct hosted zone by matching the longest matching suffix.
+    final Optional<HostedZone> matchingZone = route53.listHostedZones().getHostedZones().stream()
+        .filter(zone -> fqdnRecord.endsWith(zone.getName()))
+        .reduce(((acc, zone) -> {
+          if (zone.getName().length() > acc.getName().length()) {
+            return zone;
+          }
+          return acc;
+        }));
+
+    matchingZone.ifPresent(zone ->
+        log.info("Found matching zone {} for record {}", zone.getName(), record));
+
+    return matchingZone;
   }
 }
