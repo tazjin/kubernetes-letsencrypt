0da271e731fdce78d442c1496fe708bd2c6502b6
Vincent Ambo <tazjin@gmail.com>
2017-05-22 11:33:44 +0200

    refactor test/acme: Use mockito-kotlin

--- src/test/kotlin/in/tazj/k8s/letsencrypt/acme/CloudDnsResponderTest.kt
+++ src/test/kotlin/in/tazj/k8s/letsencrypt/acme/CloudDnsResponderTest.kt
@@ -1,29 +1,30 @@
 package `in`.tazj.k8s.letsencrypt.acme
 
-
 import com.google.cloud.Page
 import com.google.cloud.dns.Dns
 import com.google.cloud.dns.Zone
-import com.google.common.collect.ImmutableList
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
 import org.junit.Assert
 import org.junit.Test
-import org.mockito.Mockito
 
 
 class CloudDnsResponderTest {
     @Test
     fun testFindCorrectZone() {
-        val dns = Mockito.mock(Dns::class.java)
-        val page = Mockito.mock(Page::class.java)
-        val iterator = ImmutableList.of(
+        val iterator = listOf(
                 zoneOf("incorrect-1", "tazj.in."),
                 zoneOf("correct", "test.tazj.in."),
                 zoneOf("incorrect-2", "other.tazj.in.")
         ).iterator()
 
-        Mockito.`when`(page.iterateAll()).thenReturn(iterator)
+        val page: Page<Zone> = mock {
+            on { iterateAll() } doReturn (iterator)
+        }
 
-        Mockito.`when`(dns.listZones()).thenReturn(page as Page<Zone>)
+        val dns: Dns = mock {
+            on { listZones() } doReturn (page)
+        }
 
         val responder = CloudDnsResponder(dns)
         val testRecord = "_acme-challenge.some.test.tazj.in"
@@ -35,9 +36,9 @@ class CloudDnsResponderTest {
     }
 
     private fun zoneOf(name: String, dnsName: String): Zone {
-        val zone = Mockito.mock(Zone::class.java)
-        Mockito.`when`(zone.name).thenReturn(name)
-        Mockito.`when`(zone.dnsName).thenReturn(dnsName)
-        return zone
+        return mock {
+            on { getName() } doReturn (name)
+            on { getDnsName() } doReturn (dnsName)
+        }
     }
 }
