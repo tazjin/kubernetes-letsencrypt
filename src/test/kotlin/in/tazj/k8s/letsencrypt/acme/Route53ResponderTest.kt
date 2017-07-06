5278c923a41b9fdc3ce1346c08144ecacb33685d
itomaldonado <mo.maldonado@gmail.com>
2017-07-06 15:08:30 -0400

    chore route53: Add test for split-brain DNS situation

--- src/test/kotlin/in/tazj/k8s/letsencrypt/acme/Route53ResponderTest.kt
+++ src/test/kotlin/in/tazj/k8s/letsencrypt/acme/Route53ResponderTest.kt
@@ -2,6 +2,7 @@ package `in`.tazj.k8s.letsencrypt.acme
 
 import com.amazonaws.services.route53.AmazonRoute53Client
 import com.amazonaws.services.route53.model.HostedZone
+import com.amazonaws.services.route53.model.HostedZoneConfig
 import com.amazonaws.services.route53.model.ListHostedZonesResult
 import com.google.common.collect.ImmutableList
 import com.nhaarman.mockito_kotlin.doReturn
@@ -13,9 +14,12 @@ class Route53ResponderTest {
     @Test
     fun correctHostedZoneIsFound() {
         val testList = ImmutableList.of(
-                HostedZone("incorrect-1", "tazj.in.", ""),
-                HostedZone("correct", "test.tazj.in.", ""),
+                HostedZone("incorrect-1", "tazj.in.", "")
+                        .withConfig(HostedZoneConfig().withPrivateZone(false)),
+                HostedZone("correct", "test.tazj.in.", "")
+                        .withConfig(HostedZoneConfig().withPrivateZone(false)),
                 HostedZone("incorrect-2", "other.tazj.in.", "")
+                        .withConfig(HostedZoneConfig().withPrivateZone(false))
         )
 
         val client: AmazonRoute53Client = mock {
@@ -28,5 +32,30 @@ class Route53ResponderTest {
 
         Assert.assertTrue("A hosted zone is found", result.isDefined())
         Assert.assertEquals("The selected hosted zone is correct", "correct", result.get().id)
+    }
+
+    @Test
+    fun correctPublicHostedZoneIsFound() {
+        val testList = ImmutableList.of(
+                HostedZone("incorrect-1", "tazj.in.", "")
+                        .withConfig(HostedZoneConfig().withPrivateZone(true)),
+                HostedZone("private", "test.tazj.in.", "")
+                        .withConfig(HostedZoneConfig().withPrivateZone(true)),
+                HostedZone("public", "test.tazj.in.", "")
+                        .withConfig(HostedZoneConfig().withPrivateZone(false)),
+                HostedZone("incorrect-2", "other.tazj.in.", "")
+                        .withConfig(HostedZoneConfig().withPrivateZone(true))
+        )
+
+        val client: AmazonRoute53Client = mock {
+            on { listHostedZones() } doReturn (ListHostedZonesResult().withHostedZones(testList))
+        }
+
+        val responder = Route53Responder(client)
+        val testRecord = "_acme-challenge.some.test.tazj.in"
+        val result = responder.findHostedZone(testRecord)
+
+        Assert.assertTrue("A hosted zone is found", result.isDefined())
+        Assert.assertEquals("The selected hosted zone is correct", "public", result.get().id)
     }
 }
