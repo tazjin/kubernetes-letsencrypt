c4d4ca2e3ae57f7bce98061a2846191417b44d76
Vincent Ambo <tazjin@gmail.com>
2017-07-07 13:54:23 +0200

    chore build: Bump outdated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
     dependencies {
         classpath(kotlinModule("gradle-plugin"))
-        classpath("com.bmuschko:gradle-docker-plugin:3.0.8")
+        classpath("com.bmuschko:gradle-docker-plugin:3.0.9")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.15.0")
     }
 }
@@ -49,9 +49,9 @@ repositories {
 dependencies {
     compile(kotlinModule("stdlib"))
     compile(kotlinModule("reflect"))
-    compile("io.fabric8", "kubernetes-client", "2.2.13")
-    compile("com.google.code.gson", "gson", "2.8.0")
-    compile("com.amazonaws", "aws-java-sdk-route53", "1.11.119") {
+    compile("io.fabric8", "kubernetes-client", "2.5.2")
+    compile("com.google.code.gson", "gson", "2.8.1")
+    compile("com.amazonaws", "aws-java-sdk-route53", "1.11.160") {
         // Prevent AWS SDK from dragging in unwanted (unstructured) logging
         exclude(group = "commons-logging")
     }
@@ -59,16 +59,16 @@ dependencies {
     compile("org.shredzone.acme4j", "acme4j-utils", "0.10")
     compile("dnsjava", "dnsjava", "2.1.8")
     compile("com.google.cloud", "google-cloud-dns", "0.8.0")
-    compile("org.funktionale", "funktionale-option", "1.0.1")
+    compile("org.funktionale", "funktionale-option", "1.1")
 
     // Structured logging
     compile("org.slf4j", "slf4j-api", "1.7.25")
-    compile("ch.qos.logback", "logback-classic", "1.2.2")
-    compile("net.logstash.logback", "logstash-logback-encoder", "4.9")
+    compile("ch.qos.logback", "logback-classic", "1.2.3")
+    compile("net.logstash.logback", "logstash-logback-encoder", "4.11")
     compile("org.slf4j", "jcl-over-slf4j", "1.7.25")
 
     // Test dependencies
     testCompile("junit", "junit", "4.12")
-    testCompile("org.mockito", "mockito-core", "2.8.9")
-    testCompile("com.nhaarman", "mockito-kotlin", "1.4.0")
+    testCompile("org.mockito", "mockito-core", "2.8.47")
+    testCompile("com.nhaarman", "mockito-kotlin", "1.5.0")
 }
